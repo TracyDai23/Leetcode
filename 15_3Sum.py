c80ae28df58fbd8c568e60e07caba0dfbc7374fc
TracyDai23 <tracy.dai23@gmail.com>
2019-05-01 16:46:15 -0500

    Update 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -1,3 +1,6 @@
+# 2nd try with dictionary: 23.59% 
+# take-aways: rst = list(dict.fromkeys((map(tuple, rst)))) # remove duplicates from list
+
 # 1st try: time limit exceed
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -17,3 +20,21 @@ class Solution:
                         rst.append(sorted([a,b,c]))
         return rst
             
+# Use dictionary to simplify time complexity:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        l,r,rst, cl=0,0,[],[]
+        while nums:
+            if nums[0] in cl:
+                nums.pop(0)
+            else:
+                c=nums.pop(0)
+                cl.append(c)
+                lookup = {} # list value dictionary after go through the number
+                for index_1,a in enumerate(nums): # enumerate provides index and value
+                    if -c-a in lookup:
+                        rst.append(sorted([a,-c-a,c]))
+                    lookup.update({a:index_1})
+        rst = list(dict.fromkeys((map(tuple, rst)))) # remove duplicates from list
+        
+        return rst
