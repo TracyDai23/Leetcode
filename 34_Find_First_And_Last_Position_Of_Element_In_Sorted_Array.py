ddc7d0c401cea5eb3bee6c340af48dc1a272b2bf
TracyDai23 <tracy.dai23@gmail.com>
2019-05-23 18:31:50 -0500

    Update 34_Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py

--- 34_Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
+++ 34_Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
@@ -1,3 +1,39 @@
+# Corrected submission: binary search  
+# time complexity: O(log n) 86%
+#space complexity: O(1) per official solution. All work is done in place, so the overall memory usage is constant.
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) ==0:
+            return [-1,-1]
+        L,H,S,E = 0, len(nums)-1,-1,-1
+        if len(nums) == 1:
+            if nums[L]==target:
+                return [L,H]
+            else:return [-1,-1]
+        
+        while L<=H:
+            M= (L+H)//2
+            print('L,H,S,E,M:',L,H,S,E,M)
+            if nums[M] == target:
+                S=E=M
+                L,H=M-1,M+1
+                break
+            elif nums[M] < target:
+                L=M+1
+            else:
+                H=M-1
+        if S!=-1:
+            while L>=0 and nums[L] == target:
+                S=min(S,L)
+                L-=1
+            while H<= len(nums)-1 and nums[H] ==target:
+                E=max(E,H)
+                H+=1
+        return [S,E]
+        
+
+# My first attempt with index exceed issue:
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         L,H,S,E = 0, len(nums)-1,-1,-1
