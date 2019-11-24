d6f5af7f6535902c8664f7ee491e219f9d30f6d2
TracyDai23 <tracy.dai23@gmail.com>
2019-11-24 12:31:29 -0600

    Update 0692_Top_K_Frequent_Word.py

--- 2ndTrial/0692_Top_K_Frequent_Word.py
+++ 2ndTrial/0692_Top_K_Frequent_Word.py
@@ -1,5 +1,5 @@
 # 找maximum 的题， 还是需要用-value然后最后从heap里取top k， 而不需要要求heap里只有top k 个元素。 heap里对元素数量的要求是295， find median的题型
-
+# time: O(N + k*log(N)); space: O(N)
 from heapq import *
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
@@ -9,5 +9,5 @@ class Solution:
         
         for key,value in d.items():
             h.append((-value, key))
-        heapify(h)
-        return [heappop(h)[1] for _ in range(k)]
+        heapify(h) # O(N)
+        return [heappop(h)[1] for _ in range(k)] # k*O(log N)
