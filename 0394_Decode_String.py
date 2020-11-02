2b423e6171bd2da3c862d97afcf1586d78031660
TracyDai23 <tracy.dai23@gmail.com>
2020-11-01 23:12:14 -0800

    Update 0394_Decode_String.py

--- 0394_Decode_String.py
+++ 0394_Decode_String.py
@@ -1,3 +1,27 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []; curNum = 0; curString = ''
+        for c in s:
+            print("c: " , c)
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0 #这里很聪明的完成了后续的数字的更新，这样如果数字是两位数可以处理好，同时不会把之前的数据带到下一轮
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+                print("curString: ", curString, ", prevString: ", prevString)
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+            print("stack: ", stack)
+            print("curString: ", curString, "curNumber: " ,curNum)
+        return curString
+        
+
 # incorrect first try
 class Solution:
     def decodeString(self, s: str) -> str:
